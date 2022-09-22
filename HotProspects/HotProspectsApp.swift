9a4fd6264fd38ba58e148e85e39bf945c0d39233
NT <usedforfunny@gmail.com>
2022-09-22 15:58:25 +0200

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Alexandre Talatinian on 22/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
